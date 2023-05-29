4e1c48600a51784c1c066b4dfb0b2d38ae2b762b
syuhas <syuhas22@gmail.com>
2023-05-29 11:29:17 -0400

    changes application to app

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from website import create_app
 
 
-application = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    app.run(debug=True)
 
 
 ##test
